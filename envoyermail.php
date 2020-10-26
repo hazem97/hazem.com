c71b0c7a4c357d0fb4480a93de7adc0c4b210ce8
hazem97 <60571783+hazem97@users.noreply.github.com>
2020-10-26 18:56:42 +0100

    Add files via upload

--- envoyermail.php
+++ envoyermail.php
@@ -1,14 +1,21 @@
 <?php
-$mail=$_GET['mail'];
+$mail=$_POST['email'];
+$name=$_POST['name'];
+
 $pass=$_GET['pass'];
   
-  $sujet = "Password";
-  $corp = "Salut ceci est un email contient votre mot de passe";
-  $cc=$corp." ".$pass;
-  $headers = "From: kalboussihazem12@gmail.com";
-  if (mail($mail, $sujet, $cc, $headers)) {
+  $sujet =$_POST['subject'];
+
+  $corp =$_POST['message'];
+  $cc=$corp;
+   
+  $headers = "From:".$mail;
+  if (mail("kalboussihazem67@gmail.com", $sujet, $cc, $headers)) {
     echo "success";
   } else {
     echo "echec";
   }
 ?>
+<html>
+<a href="https://hazem97.github.io/hazem.com/hazem.html">retour</a>
+</html>
