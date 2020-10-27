afcd7f875e070d281bde10da13ee6ef1c2190001
hazem97 <60571783+hazem97@users.noreply.github.com>
2020-10-27 01:27:26 +0100

    Add files via upload

--- envoyermail.php
+++ envoyermail.php
@@ -1,21 +1,21 @@
 <?php
-$mail=$_POST['email'];
-$name=$_POST['name'];
-
-$pass=$_GET['pass'];
-  
-  $sujet =$_POST['subject'];
-
-  $corp =$_POST['message'];
-  $cc=$corp;
-   
-  $headers = "From:".$mail;
-  if (mail("kalboussihazem67@gmail.com", $sujet, $cc, $headers)) {
-    echo "success";
-  } else {
-    echo "echec";
-  }
+$destinataire = 'kalboussihazem07@gamil.com';
+// Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
+$expediteur = 'kalboussihazem67@gmail.com';
+$copie = 'adresse@fai.com';
+$copie_cachee = 'adresse@fai.com';
+$objet = 'Test'; // Objet du message
+$headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
+$headers .= 'Reply-To: '.$expediteur."\n"; // Mail de reponse
+$headers .= 'From: "Nom_de_expediteur"<'.$expediteur.'>'."\n"; // Expediteur
+       
+$message = 'Un Bonjour de Developpez.com!';
+if (mail($destinataire, $objet, $message, $headers)) // Envoi du message
+{
+    echo 'Votre message a bien été envoyé ';
+}
+else // Non envoyé
+{
+    echo "Votre message n'a pas pu être envoyé";
+}
 ?>
-<html>
-<a href="https://hazem97.github.io/hazem.com/hazem.html">retour</a>
-</html>
